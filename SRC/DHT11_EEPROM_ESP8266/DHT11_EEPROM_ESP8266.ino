3d4e0cf296a06dae52ad82be4f53ffa2a19fb85e
jarfenoni <jeremias.arfenoni@gmail.com>
2015-11-30 13:12:17 -0300

    depuracion arduino code
    Este codigo ha estado funcionando por mas de 40 min sin problemas...

--- SRC/DHT11_EEPROM_ESP8266/DHT11_EEPROM_ESP8266.ino
+++ SRC/DHT11_EEPROM_ESP8266/DHT11_EEPROM_ESP8266.ino
@@ -1,68 +1,61 @@
-#include "DHT.h" //cargamos la librería DHT
-#define DHTPIN 2 //Seleccionamos el pin en el que se //conectará el sensor
-#define DHTTYPE DHT11 //Se selecciona el DHT11 (hay //otros DHT)
-DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino para comunicarse con el sensor
+#include "DHT.h"              //cargamos la librería DHT
+#define DHTPIN 2              //Seleccionamos el pin en el que se //conectará el sensor
+#define DHTTYPE DHT11         //Se selecciona el DHT11 (hay //otros DHT)
+DHT dht(DHTPIN, DHTTYPE);     //Se inicia una variable que será usada por Arduino para comunicarse con el sensor
 
 #include <SoftwareSerial.h>
 #include <stdlib.h>
 
 SoftwareSerial ser(10, 11);
 //asignacion de pines:
-//#define RXDATA    10      // SoftwareSerial receive pin (RED LED connected).
-//#define TXDATA    11      // SoftwareSerial transmit pin.
-#define HRESET    8      // Pin to hardware Reset the ESP8266
-//#define ANATEMP   1      // Analog channel 1 for temperature measurement.
+// RXDATA    10               // SoftwareSerial pin de recepción.
+// TXDATA    11               // SoftwareSerial pin de transmisión.
+#define HRESET    8           // Pin para hardware Reset del módulo ESP8266
+
 
 
 #include <EEPROM.h>
 int addr = 0;
-int address = 0;
 
 
-volatile float h;
-volatile float t ;
+volatile float HUMEDAD;
+volatile float TEMPERATURA ;
 volatile byte val;
 volatile byte value;
 int aux =0;
 
 //Parametros de red y direccion IP del servidor donde enviamos los datos:
-#define mySSID        "IT10"          //    "labsenales"         "electronica"      //WiFi SSID.
-#define myPASS        "coopit10kit"   // "esunsecreto"   "laboratorio"  //Password.
-#define myCHL     "6"             //numero de canal
-#define myECN     "4"             //ecn: (0 OPEN) (1 WEP) (2 WPA_PSK) (3 WPA2_PSK) (4 WPA_WPA2_PSK)
+#define mySSID        "IT10"            //    "labsenales"          "electronica"       //WiFi SSID.
+#define myPASS        "coopit10kit"     //    "esunsecreto"         "laboratorio"       //Password.
+#define myCHL     "6"                   //numero de canal
+#define myECN     "4"                   //ecn: (0 OPEN) (1 WEP) (2 WPA_PSK) (3 WPA2_PSK) (4 WPA_WPA2_PSK)
 //AT+CWSAP=mySSID,myPASS,myCHL,myECN
 
-#define serverIP  "184.106.153.149"   //IP de ThingSpeak.com
-#define ChannKEY  "66L8D19CBNDFLPDA"  //ThingSpeak channel key.
+#define serverIP  "184.106.153.149"     //IP de ThingSpeak.com
+#define ChannKEY  "66L8D19CBNDFLPDA"    //ThingSpeak channel key.
 
 #define updField  "1"                   //Updating Field( 1,2,3,4,5...) ThingSpeak
 #define SPERIOD   30000                 //Periodo de muestreo (milisegundos).
-#define MAXFAILS  100                    //Numero de fallas para RESET del ESP8266.
+#define MAXFAILS  100                   //Numero de fallas para RESET del ESP8266.
 
 //Concatenamos los string messages:
 #define soft_RST  "AT+RST\r\n"
-#define mode_MSG  "AT+CWMODE=3\r\n"       //(1 station), (2 Acces Point), (3 Ambos)
-#define join_MSG  "AT+CWJAP=\"" mySSID "\",\"" myPASS "\"\r\n"      //enviar datos para vincularse al AP
+#define mode_MSG  "AT+CWMODE=3\r\n"                                     // (1 Estación), (2 Punto de acceso AP), (3 Ambos)
+#define join_MSG  "AT+CWJAP=\"" mySSID "\",\"" myPASS "\"\r\n"          // enviar datos para vincularse al AP
 //#define join_MSG  "AT+CWSAP=\"" mySSID "\",\"" myPASS "\",\"" myCHL "\",\"" myECN "\"\r\n" //enviar datos para vincularse al AP AT+CWSAP=mySSID,myPASS,myCHL,myECN
-#define conn_MSG  "AT+CIPSTART=\"TCP\",\"" serverIP "\",80\r\n"     //TCP, ThingSpeak, port 80.
-#define send_MSG  "AT+CIPSEND=46\r\n"     //Send data, size=46 (4 bytes para el valor)                     
-#define upda_MSG  "GET /update?key=" ChannKEY "&field" updField "=" //Updating Field data.
-#define clos_MSG  "AT+CIPCLOSE\r\n"       //Close TCP connection.
-unsigned long interval=SPERIOD;     // the time we need to wait
-unsigned long previousMillis=0;  // millis() returns an unsigned long.
+#define conn_MSG  "AT+CIPSTART=\"TCP\",\"" serverIP "\",80\r\n"         // TCP, ThingSpeak, puerto 80.
+unsigned long interval=SPERIOD;                                         // El tiempo que necesitamos esperar
+unsigned long previousMillis=0;                                         // millis() retorna un unsigned long.
 
-String apiKey = "66L8D19CBNDFLPDA";
+String apiKey = "66L8D19CBNDFLPDA";                                     //key de mi canal en thingspeak
 
 
 int i,j;
-int fails=101;           // cargar failures>MAXFAILS para generar un RESET al inicio.
-//unsigned int temp;       // Use positive range values (0-32768)
-String newVal;//="00.0";   // Four characters (fixed format number).
+int fails=101;                  // cargar failures>MAXFAILS para generar un RESET al inicio.
+String newVal;//="00.0";        // Four characters (fixed format number).
 String cmd;
 float temp;
 String getStr;
-//SoftwareSerial ser(RXDATA, TXDATA);   // Creates SoftwareSerial channel.
-
 int contador = 0;
 
 
@@ -70,26 +63,33 @@ int contador = 0;
 
 
 void setup() {
-  Serial.begin(19200); //Se inicia la comunicación serial 
-  dht.begin(); //Se inicia el sensor
-  ser.begin(19200);
-  pinMode(HRESET, OUTPUT);
-  digitalWrite(HRESET,HIGH);
+  Serial.begin(19200);              //Se inicia la comunicación serial 
+  dht.begin();                      //Se inicia el sensor
+  ser.begin(19200);                 //Se inicia el puerto serie por sofware
+  pinMode(HRESET, OUTPUT);          //Declaro un pin como salida para hacer el hardware reset del ESP
+  digitalWrite(HRESET,HIGH);        
   initESP();                        // ESP8266 init.
 }
 
 
 void loop() {
-h = dht.readHumidity();       //Se lee la humedad
-t = dht.readTemperature();    //Se lee la temperatura
+HUMEDAD = dht.readHumidity();                   //Se lee la humedad
+TEMPERATURA = dht.readTemperature();            //Se lee la temperatura
+if((HUMEDAD == NAN) || (TEMPERATURA == NAN))    //si la lectura falla el modulo devuelve NAN
+{
+  Serial.print("Reinicio del modulo DHT11");
+  dht.begin();                                  //Se inicia el sensor
+  HUMEDAD = dht.readHumidity();                 //Se lee la humedad
+  TEMPERATURA = dht.readTemperature();          //Se lee la temperatura
+}
 
-val = (byte) h;               //casteo para poder escribir en EEPROM
-EEPROM.write(addr, val);      //escribe en la direccion addr el entero val (t casteado a int)
+val = (byte) HUMEDAD;                           //casteo para poder escribir en EEPROM
+EEPROM.write(addr, val);                        //escribe en la direccion addr el entero val (t casteado a int)
 
-val = (byte) t;
-EEPROM.write(addr+1, val);    //escribe en la direccion addr el entero val (h casteado a int)
+val = (byte) TEMPERATURA;
+EEPROM.write(addr+1, val);                      //escribe en la direccion addr el entero val (h casteado a int)
 
-                              //Se imprimen las variables (previamente guardadas en la EEPROM)
+//Se imprimen las variables (previamente guardadas en la EEPROM)
 Serial.println("Humedad: "); 
 value = EEPROM.read(addr);
 aux = (int) value;
@@ -97,7 +97,6 @@ Serial.print(aux, DEC);
 Serial.println();
 
 Serial.println("Temperatura: ");
-address++;
 value = EEPROM.read(addr+1);
 aux = (int) value;
 Serial.print(aux, DEC);
@@ -118,14 +117,16 @@ Serial.println();
 
 
 // Preparo GET string
+
   getStr = "GET /update?api_key=";
   getStr += apiKey;
   getStr +="&field2=";
   getStr += String(strTemp);
   getStr += "\r\n\r\n";
 
+//envia el largo del string, y luego envia el string a thingspeak
 
-  Enviar_largo_dato();   //envia el largo del string, y luego envia el string a thingspeak
+  Enviar_largo_dato();   
   
 
 
@@ -164,11 +165,10 @@ void sendDebug(String cmd)
   
 }
 //________________________________________________________________________________________________________________________
-void initESP(){    //con esta funcion se resetea el modulo, se configura como estación y se conecta a un AP.
+void initESP(){                     //con esta funcion se resetea el modulo, se configura como estación y se conecta a un AP.
   while(1){
     if (fails > MAXFAILS)           //------ reset ESP8266. 
     {
-      delay(2000); //este delay esta para poder debuggiar
       Serial.println("Hardware RESET");
       digitalWrite(HRESET,LOW);
       
@@ -273,14 +273,11 @@ void initESP(){    //con esta funcion se resetea el modulo, se configura como es
       if(waitChar('K',10000))
       {
         Serial.println("OK");
+        fails=101; // la proxima vez que envie a configurar el ESP hará un hardware reset
         return;
       } //Esperar por 'K' de "OK". para salir del setup                  Reemplace ok por k
       delay(2000);
-//-----------------------------------------------------------------------------
-      
-
-
-      
+//-----------------------------------------------------------------------------      
     }
     fails++;
   }
@@ -325,19 +322,28 @@ void Enviar_largo_dato()
     Serial.println("AT+CIPCLOSE");
     //-----------------------------------------------------------------------------
     // reinicio el modulo
-    initESP();
-/*    Serial.println("Se reiniciara el modulo");
-    
+   // initESP();
+    Serial.println("Se reiniciara el modulo");
+
+      Serial.println("Hardware RESET");
+      digitalWrite(HRESET,LOW);
+      delay(3000);
+      digitalWrite(HRESET,HIGH);
+      
+      delay(3000);
       sendDebug("AT");
-      delay(2000);
-      int contador = 0;
-      while ((!ser.find("OK")) && (contador < 100))
+      delay(2000);      
+      while (!ser.find("OK"))
       {
         Serial.print(".");
         delay(300);
         contador++;
+        if(contador >= 10)
+        {
+          contador=0;
+          sendDebug("AT");
+        }
       };
-      contador = 0;
       Serial.println("OK");   //es lo que responderia el ESP (lo simulo para verlo en la consola)
       
            
@@ -350,7 +356,7 @@ void Enviar_largo_dato()
         return;
       }
       delay(2000);
-      */
+ /*     */
 //-----------------------------------------------------------------------------
   }
 }
